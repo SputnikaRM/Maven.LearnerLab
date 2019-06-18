9755773672219a61fd3f8b3633cf114acddcab52
sputnika <sputnika91@gmail.com>
2019-06-18 17:10:38 -0400

    zpw classish

--- src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
+++ src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
@@ -1,4 +1,33 @@
 package io.zipcoder.interfaces;
 
 public class ZipCodeWilmington {
+    Students students= Students.getInstance();
+    Instructors instructors=Instructors.getINSTANCE();
+
+    private final static ZipCodeWilmington zpw = new ZipCodeWilmington();
+    private ZipCodeWilmington () {
+
+    }
+    public static ZipCodeWilmington getZPW (){
+        return zpw;
+    }
+
+    public void hostLecture (Teacher teacher, double numberOfHours) {
+        teacher.lecture((Learner[])students.toArray(),numberOfHours );
+
+
+
+    }
+
+    public void hostLecture (long id, double numberOfHours){
+        instructors.findById(id);
+        Person teach = instructors.findById(id);
+        
+
+
+    }
+
+
+
+
 }
