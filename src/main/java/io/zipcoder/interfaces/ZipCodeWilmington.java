34c692b639c9682bb0d7e9d17ef0d6e3c210cc39
Joanna Hsiao <chsiao58@gmail.com>
2019-06-19 10:31:29 -0400

    organized the code and finished second hostLecture function

--- src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
+++ src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
@@ -8,29 +8,24 @@ public class ZipCodeWilmington {
     Instructors instructors=Instructors.getINSTANCE();
 
     private final static ZipCodeWilmington zpw = new ZipCodeWilmington();
-    private ZipCodeWilmington () {
 
-    }
+    private ZipCodeWilmington() {}
+
     public static ZipCodeWilmington getZPW (){
         return zpw;
     }
 
-    public void hostLecture (Teacher teacher, double numberOfHours) {
+    public void hostLecture(Teacher teacher, double numberOfHours) {
         teacher.lecture((Learner[])students.toArray(),numberOfHours );
-
-
-
     }
 
-    public void hostLecture (long id, double numberOfHours){
+    public void hostLecture(long id, double numberOfHours) {
         instructors.findById(id);
         Person teach = instructors.findById(id);
-
-
-
+        hostLecture((Teacher)teach, numberOfHours);
     }
 
-    public Map<Student, Double> getStudyMap () {
+    public Map<Student,Double> getStudyMap() {
         HashMap<Student,Double> map = new HashMap<>();
 
         for(Object student: students){
