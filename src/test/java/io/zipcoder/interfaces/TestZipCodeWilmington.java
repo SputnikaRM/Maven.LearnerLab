f63b991a7da3a34701061b633a02d59ffddbff8c
sputnika <sputnika91@gmail.com>
2019-06-19 10:16:22 -0400

    testzipwlimington created

--- src/test/java/io/zipcoder/interfaces/TestZipCodeWilmington.java
+++ src/test/java/io/zipcoder/interfaces/TestZipCodeWilmington.java
@@ -1,7 +1,16 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TestZipCodeWilmington {
+
+    @Test
+
+    public void testHostLecture (){
+
+    }
+
 
 }
